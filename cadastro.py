b6b158bb623efb691f1afa3dc9b64c928b8f376c
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-09-25 12:12:19 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -2,11 +2,12 @@ import random
 import re
 def input_cpf():
     while True:
-        cpf = input("CPF (11 dígitos): ")
-        if cpf.isdigit() and len(cpf) == 11:
-            return cpf
-        print("CPF inválido! Digite exatamente 11 números.")
-
+        cpf = input("CPF (com ou sem pontuação): ")
+        cpf_limpo = cpf.replace('.', '').replace('-', '')
+        if cpf_limpo.isdigit() and len(cpf_limpo) == 11:
+            return cpf_limpo  
+        
+        print("CPF inválido! Digite 11 números (com ou sem pontuação).")
 def input_data():
     while True:
         data = input("Data de Nascimento (dd-mm-aaaa): ")
@@ -25,13 +26,13 @@ dados = {
     "Nome": input("Nome: "),
     "Data de Nascimento": input_data(),
     "Telefone": input_tel(),
-    "Endereço": input("Endereço: "),
+    "Endereço(rua)": input("Endereço: "),
     "CPF": input_cpf()
 }
 print("\n--- Cadastro ---")
 print("Nome:", dados["Nome"])
 print("Data de Nascimento:", dados["Data de Nascimento"])
 print("Telefone:", dados["Telefone"])
-print("Endereço:", dados["Endereço"])
+print("Endereço:", dados["Endereço(rua)"])
 print("CPF:", dados["CPF"])
 print("ID gerado:", random.randint(1000, 9999))
