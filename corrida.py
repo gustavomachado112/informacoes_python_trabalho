9da7f0065c1c43285b8bb1d7240f88ba6af8d806
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-09-25 16:22:52 -0300

    Create corrida.py

---
+++ corrida.py
@@ -0,0 +1,29 @@
+import random
+
+print(" CADASTRO DE CORRIDA ")
+corrida_id = random.randint(10000, 99999)
+circuito_id = random.randint(10000, 99999)
+
+while True:
+    nome = input("Nome da corrida: ")
+    if nome:  
+        break
+    print("Nome é obrigatório!")
+
+while True:
+    local = input("Local da corrida: ")
+    if local:
+        break
+    print("Local é obrigatório!")
+    
+while True:
+    data = input("Data (dd-mm-aaaa): ")
+    if data and len(data) == 10:
+        break
+    print("Data é obrigatória! Use dd-mm-aaaa")
+print("\nCorrida ID:", corrida_id)
+print("Nome:", nome)
+print("Local:", local)
+print("Data:", data)
+print("Circuito ID:", circuito_id)
+print("\nCorrida cadastrada!")
