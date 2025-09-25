1ea444796866db799ad8f214660338089141a445
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-09-25 15:25:19 -0300

    Create participacao.py

---
+++ participacao.py
@@ -0,0 +1,22 @@
+import random
+print(" CADASTRO DE PARTICIPAÇÃO ")
+
+participacao_id = random.randint(10000, 99999)
+piloto_id = random.randint(10000, 99999)
+corrida_id = random.randint(10000, 99999)
+
+while True:
+    try:
+        posicao_final = int(input("Posição final (>= 1): "))
+        if posicao_final >= 1:
+            break
+        else:
+            print("Posição deve ser 1 ou maior!")
+    except:
+        print("Digite um número válido!")
+print("\n=== INFORMAÇÕES ===")
+print("Participacao ID:", participacao_id)
+print("Piloto ID:", piloto_id)
+print("Corrida ID:", corrida_id)
+print("Posição Final:", posicao_final)
+print("\nParticipação cadastrada!")
