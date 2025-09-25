41bdf046f7628d014c323ffb514143d9f3b1a832
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-09-24 22:16:29 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,30 @@
+import random, re
+
+def input_email():
+    while True:
+        e = input("Email: ")
+        if re.match(r"[^@]+@[^@]+\.[^@]+", e):
+            return e
+        print("Email inválido!")
+
+def input_senha():
+    while True:
+        s = input("Senha (mín 6 chars): ")
+        if len(s) >= 6:
+            return s
+        print("Senha curta demais!")
+
+
+user_id = random.randint(10000, 99999)
+
+
+print("\n--- Criar login ---")
+email = input_email()
+senha = input_senha()
+login_id = random.randint(10000, 99999)
+
+print("\n--- IDs Gerados ---")
+print("User ID:", user_id)
+print("Login ID:", login_id)
+print("Email:", email)
+print("Senha:", '*' * len(senha)) 
