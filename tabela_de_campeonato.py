dedb3a3bea57319af613967d01e7eb163ee0af38
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-10-01 08:28:01 -0300

    Update tabela_de_campeonato.py

--- tabela_de_campeonato.py
+++ tabela_de_campeonato.py
@@ -1,25 +1,24 @@
 import random
-
-print(" CADASTRO DE CAMPEONATO ")
-construct_id = random.randint(10000, 99999)
+print(" Cadastro de Campeonato ")
+construtor_id = random.randint(10000, 99999)
 equipe_id = random.randint(10000, 99999)
 piloto_id = random.randint(10000, 99999)
 resultado_id = random.randint(10000, 99999)
 
 while True:
     pontos_equipe = int(input("Pontos da equipe: "))
-    if pontos_equipe >= 0:
+    if pontos_equipe >=0:
         break
-    print("Pontos devem ser zero ou maior!")
+    print("Pontos devem ser computados de acordo com a corrida")
 
 while True:
     pontos_piloto = int(input("Pontos do piloto: "))
     if pontos_piloto >= 0:
         break
-    print("Pontos devem ser zero ou maior!")
+    print("Pontos devem ser computados de acordo com a corrida")
 
-print("\n=== INFORMAÇÕES ===")
-print("Construct ID:", construct_id)
+print(" Informacoes")
+print("Construtor ID:", construtor_id)
 print("Equipe ID:", equipe_id)
 print("Piloto ID:", piloto_id)
 print("Resultado ID:", resultado_id)
