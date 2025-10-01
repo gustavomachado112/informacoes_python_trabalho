7eff02709f01b1258d221d097af07192c3be544b
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-10-01 08:52:36 -0300

    Update carros.py

--- carros.py
+++ carros.py
@@ -6,17 +6,16 @@ def input_ano():
             if 1900 <= a <= 2100:  
                 return a
             else:
-                print("Ano inválido!")
+                print("Ano invalido!")
         except:
-            print("Digite um número válido!")
-
+            print("Digite um numero valido!")
 
 carro_id = random.randint(10000, 99999)
 equipe_id = random.randint(1000, 9999)  
 modelo = input("Modelo do carro: ")
 ano = input_ano()
 
-print("\n--- KOTB ---")
+print("\n Carro")
 print("Carro ID:", carro_id)
 print("Modelo:", modelo)
 print("Ano:", ano)
