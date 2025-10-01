5f349501165996721193c8133109230838ad237a
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-10-01 08:35:51 -0300

    Update sesssoes_de_treino.py

--- sesssoes_de_treino.py
+++ sesssoes_de_treino.py
@@ -5,7 +5,7 @@ def input_data():
         d = input("Data da sessão (dd-mm-aaaa): ")
         if re.match(r"^\d{2}-\d{2}-\d{4}$", d):
             return d
-        print("Formato inválido! Use dd-mm-aaaa.")
+        print("Formato inválido Use dd-mm-aaaa.")
 
 def input_duracao():
     while True:
@@ -14,7 +14,7 @@ def input_duracao():
             if duracao > 0:
                 return duracao
             else:
-                print("Duração deve ser maior que zero!")
+                print("Duração deve ser maior que zero.")
         except:
             print("Digite um número válido.")
 treino_id = random.randint(10000, 99999)
@@ -22,11 +22,8 @@ corrida_id = random.randint(10000, 99999)
 data_sessao = input_data()
 duracao_sessao = input_duracao()
 
-
-print("\n--- Sessão de Treino ---")
+print("\n Sessão de Treino ")
 print("Treino ID:", treino_id)
 print("Corrida ID:", corrida_id)
 print("Data da Sessão:", data_sessao)
 print("Duração (minutos):", duracao_sessao)
-
-
